ed56139955fc7ea4a55c0be443dfc7af9398e13d
Armon Dadgar <armon.dadgar@gmail.com>
2011-11-30 13:31:09 -0800

    Version bump

--- pybloomd.py
+++ pybloomd.py
@@ -2,7 +2,7 @@
 This module implements a client for the BloomD server.
 """
 __all__ = ["BloomdError", "BloomdConnection", "BloomdClient", "BloomdFilter"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 import logging
 import socket
 import errno
